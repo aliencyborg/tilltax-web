dd19b9dccd3e1d7d878f39024bf27880e364c69b
Benjamin Rosas <ballPointPenguin@gmx.us>
2017-05-25 09:02:34 -0500

    navbar and footer

---
+++ tests/integration/components/tt-footer-test.js
@@ -0,0 +1,33 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import textLines from 'tilltax/tests/helpers/text-lines';
+
+moduleForComponent('tt-footer', 'Integration | Component | tt footer', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{tt-footer}}`);
+
+  let expectedLines = [
+    '© 2017 Shelly J Till',
+    'Till Tax Accounting & Financial Services LLC |',
+    '17446 Hiawatha Beach Drive NE, Ham Lake, MN 55304 |',
+    'ShellyJTill@TillTax.com'
+  ];
+
+  assert.deepEqual(textLines(this.$()), expectedLines);
+
+  // Template block usage:
+  this.render(hbs`
+    {{#tt-footer}}
+      template block text
+    {{/tt-footer}}
+  `);
+
+  assert.deepEqual(textLines(this.$()), expectedLines);
+});
